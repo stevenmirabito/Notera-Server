202a1f3a48cf6fae983e0b452914606777652898
William Stevens <wastevensv@gmail.com>
2016-01-31 00:07:11 -0500

    Database working. Next, web interface.

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from application.database import session
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<h1 style='color:red'>Hello There!</h1>"
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    session.remove()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
---
+++ application/models.py
@@ -0,0 +1,62 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, Table
+from sqlalchemy.orm import relationship
+from application.database import Base
+
+student_course_table = Table('student_course_rel', Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id')),
+    Column('course_id', Integer, ForeignKey('courses.id'))
+)
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    realname = Column(String(120))
+
+    notes = relationship("Note", backref="student")
+    courses = relationship(
+        "Course",
+        secondary=student_course_table,
+        back_populates="students")
+
+    def __init__(self, username=None, realname=None):
+        self.username = username
+        self.realname = realname
+
+    def __repr__(self):
+        return '<Student %r>' % (self.username)
+
+class Course(Base):
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True)
+    coursename = Column(String(120), unique=True)
+    professor = Column(String(120), unique=False)
+
+    notes = relationship("Note", backref="course")
+    students = relationship(
+        "Student",
+        secondary=student_course_table,
+        back_populates="courses")
+
+    def __init__(self, coursename=None, professor=None):
+        self.coursename = coursename
+        self.professor = professor
+
+    def __repr__(self):
+        return '<Course %r>' % (self.coursename)
+
+class Note(Base):
+    __tablename__ = 'notes'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(120))
+    body = Column(Text())
+
+    student_id = Column(Integer, ForeignKey('students.id'))
+    course_id = Column(Integer, ForeignKey('courses.id'))
+
+    def __init__(self, title=None, body=None):
+        self.title = title
+        self.username = body
+
+    def __repr__(self):
+        return '<Note %r>' % (self.title)
